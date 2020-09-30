01e2da5303346f3e29f4bc9f38a235413d4c009c
Ripose <changeit@example.com>
2020-09-29 19:50:27 -0700

    Added enum dictionary type

---
+++ src/dictionary/dictionarytype.h
@@ -0,0 +1,22 @@
+#ifndef DICTIONARYTYPE_H
+#define DICTIONARYTYPE_H
+
+enum DictionaryType {
+	
+	/**
+	 * Normal Japanese dictionary
+	 */
+	JAPANESE_DEFAULT,
+	
+	/**
+	 * Japanese names dictionary
+	 */
+	JAPANESE_NAMES,
+	
+	/**
+	 * Chinese dictionary
+	 */
+	CHINESE
+}
+
+#endif // DICTIONARYTYPE_H
