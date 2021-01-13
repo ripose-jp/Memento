50095d8758c7cab64bc47888b9dd537a44faabbb
Ripose <ripose@protonmail.com>
2021-01-13 15:09:39 -0800

    Added help window

---
+++ src/gui/widgets/ankisettingshelp.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2020 Ripose
+//
+// This file is part of Memento.
+//
+// Memento is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 of the License.
+//
+// Memento is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Memento.  If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include "ankisettingshelp.h"
+#include "ui_ankisettingshelp.h"
+
+AnkiSettingsHelp::AnkiSettingsHelp(QWidget *parent) 
+    : QWidget(parent), m_ui(new Ui::AnkiSettingsHelp)
+{
+    m_ui->setupUi(this);
+}
+
+AnkiSettingsHelp::~AnkiSettingsHelp()
+{
+    delete m_ui;
+}
