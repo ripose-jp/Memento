6387143746fa1048ff2d7592deb51f843355bc96
Ripose <ripose@protonmail.com>
2021-02-26 00:33:36 -0800

    Added header file to transcode

--- src/ffmpeg/transcode_aac.c
+++ src/ffmpeg/transcode_aac.c
@@ -23,12 +23,12 @@
  * Simple audio converter
  *
  * @example transcode_aac.c
- * Convert an input audio file to AAC in an MP4 container using FFmpeg.
+ * Convert an input audio file to AAC using FFmpeg.
  * Formats other than MP4 are supported based on the output file extension.
  * @author Andreas Unterweger (dustsigns@gmail.com)
  */
 
-#include <stdio.h>
+#include "transcode_aac.h"
 
 #include "libavformat/avformat.h"
 #include "libavformat/avio.h"
