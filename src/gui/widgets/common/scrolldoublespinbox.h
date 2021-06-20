171c3b9a0fa373d82c366655dfbc6cb6e243117b
Ripose <ripose@protonmail.com>
2021-06-20 14:26:30 -0700

    Prevents spin boxes from stealing focus when scrolling

---
+++ src/gui/widgets/common/scrolldoublespinbox.h
@@ -0,0 +1,38 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2021 Ripose
+//
+// This file is part of Memento.
+//
+// Memento is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 2 of the License.
+//
+// Memento is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Memento.  If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <QDoubleSpinBox>
+
+#include <QWheelEvent>
+
+class ScrollDoubleSpinBox : public QDoubleSpinBox
+{
+    Q_OBJECT
+
+public:
+    ScrollDoubleSpinBox(QWidget *parent = nullptr) : QDoubleSpinBox(parent)
+    {
+        setFocusPolicy(Qt::StrongFocus);
+    }
+
+protected:
+    void wheelEvent(QWheelEvent *event) override
+        { event->ignore(); }
+};
