60d0b07b37d12e948a91d45e873ef0261d15a881
Ripose <ripose@protonmail.com>
2021-06-28 13:43:14 -0700

    playercontrols: Add documentation, update to 80 char width

--- src/gui/widgets/playercontrols.cpp
+++ src/gui/widgets/playercontrols.cpp
@@ -21,15 +21,11 @@
 #include "playercontrols.h"
 #include "ui_playercontrols.h"
 
-#include "common/sliderjumpstyle.h"
 #include "../../util/globalmediator.h"
 #include "../playeradapter.h"
+#include "common/sliderjumpstyle.h"
 
-#define SECONDS_IN_MINUTE   60
-#define SECONDS_IN_HOUR     3600
-#define FILL_SPACES         2
-#define BASE_TEN            10
-#define FILL_CHAR           '0'
+/* Begin Constructor/Destructor */
 
 PlayerControls::PlayerControls(QWidget *parent) 
     : QWidget(parent), 
@@ -43,7 +39,7 @@ PlayerControls::PlayerControls(QWidget *parent)
     m_ui->layoutButtons->setSpacing(2);
 #endif
 
-    refreshTheme();
+    initTheme();
     setCursor(Qt::ArrowCursor);
 
     GlobalMediator *mediator = GlobalMediator::getGlobalMediator();
@@ -64,24 +60,70 @@ PlayerControls::PlayerControls(QWidget *parent)
             }
         }
     );
-    connect(m_ui->sliderProgress,      &QSlider::valueChanged, mediator, &GlobalMediator::controlsPositionChanged, Qt::QueuedConnection);
-    connect(m_ui->sliderVolume,        &QSlider::valueChanged, mediator, &GlobalMediator::controlsVolumeChanged);
+    connect(
+        m_ui->sliderProgress, &QSlider::valueChanged,
+        mediator,             &GlobalMediator::controlsPositionChanged, 
+        Qt::QueuedConnection
+    );
+    connect(
+        m_ui->sliderVolume, &QSlider::valueChanged,
+        mediator,           &GlobalMediator::controlsVolumeChanged
+    );
 
-    connect(m_ui->buttonPlay,          &QToolButton::clicked, this,      &PlayerControls::pauseResume);
-    connect(m_ui->buttonSeekForward,   &QToolButton::clicked, mediator,  &GlobalMediator::controlsSeekForward);
-    connect(m_ui->buttonSeekBackward,  &QToolButton::clicked, mediator,  &GlobalMediator::controlsSeekBackward);
-    connect(m_ui->buttonSkipForward,   &QToolButton::clicked, mediator,  &GlobalMediator::controlsSkipForward);
-    connect(m_ui->buttonSkipBackward,  &QToolButton::clicked, mediator,  &GlobalMediator::controlsSkipBackward);
-    connect(m_ui->buttonFullscreen,    &QToolButton::clicked, this,      &PlayerControls::toggleFullscreen);
-    connect(m_ui->buttonToggleSubList, &QToolButton::clicked, mediator,  &GlobalMediator::controlsSubtitleListToggled);
+    connect(
+        m_ui->buttonPlay, &QToolButton::clicked,
+        this,             &PlayerControls::togglePause
+    );
+    connect(
+        m_ui->buttonSeekForward, &QToolButton::clicked,
+        mediator,                &GlobalMediator::controlsSeekForward
+    );
+    connect(
+        m_ui->buttonSeekBackward, &QToolButton::clicked,
+        mediator,                 &GlobalMediator::controlsSeekBackward
+    );
+    connect(
+        m_ui->buttonSkipForward, &QToolButton::clicked,
+        mediator,                &GlobalMediator::controlsSkipForward
+    );
+    connect(
+        m_ui->buttonSkipBackward, &QToolButton::clicked,
+        mediator,                 &GlobalMediator::controlsSkipBackward
+    );
+    connect(
+        m_ui->buttonFullscreen, &QToolButton::clicked,
+        this,                   &PlayerControls::toggleFullscreen
+    );
+    connect(
+        m_ui->buttonToggleSubList, &QToolButton::clicked,
+        mediator,                  &GlobalMediator::controlsSubtitleListToggled
+    );
 
     /* Slots */
-    connect(mediator, &GlobalMediator::playerPauseStateChanged, this, &PlayerControls::setPaused);
-    connect(mediator, &GlobalMediator::playerFullscreenChanged, this, &PlayerControls::setFullscreen);
-    connect(mediator, &GlobalMediator::playerVolumeChanged,     this, &PlayerControls::setVolume);
-    connect(mediator, &GlobalMediator::playerDurationChanged,   this, &PlayerControls::setDuration);
-    connect(mediator, &GlobalMediator::playerPositionChanged,   this, &PlayerControls::setPosition);
-    connect(mediator, &GlobalMediator::requestThemeRefresh,     this, &PlayerControls::refreshTheme);
+    connect(
+        mediator, &GlobalMediator::playerPauseStateChanged,
+        this,     &PlayerControls::setPaused
+    );
+    connect(
+        mediator, &GlobalMediator::playerFullscreenChanged,
+        this,     &PlayerControls::setFullscreen
+    );
+    connect(
+        mediator, &GlobalMediator::playerVolumeChanged,
+        this,     &PlayerControls::setVolume
+    );
+    connect(
+        mediator, &GlobalMediator::playerDurationChanged,
+        this,     &PlayerControls::setDuration
+    );
+    connect(
+        mediator, &GlobalMediator::playerPositionChanged,
+        this,     &PlayerControls::setPosition
+    );
+    connect(
+        mediator, &GlobalMediator::requestThemeRefresh,
+        this,     &PlayerControls::initTheme
+    );
 }
 
 PlayerControls::~PlayerControls()
@@ -90,27 +132,62 @@ PlayerControls::~PlayerControls()
     delete m_ui;
 }
 
-void PlayerControls::refreshTheme()
+/* End Constructor/Destructor */
+/* Begin Initializers */
+
+void PlayerControls::initTheme()
 {
     IconFactory *factory = IconFactory::create();
-    m_ui->buttonPlay->         setIcon(factory->getIcon(m_paused ? IconFactory::Icon::play : IconFactory::Icon::pause));
-    m_ui->buttonFullscreen->   setIcon(factory->getIcon(IconFactory::Icon::fullscreen));
-    m_ui->buttonSeekBackward-> setIcon(factory->getIcon(IconFactory::Icon::seek_backward));
-    m_ui->buttonSeekForward->  setIcon(factory->getIcon(IconFactory::Icon::seek_forward));
-    m_ui->buttonSkipBackward-> setIcon(factory->getIcon(IconFactory::Icon::skip_backward));
-    m_ui->buttonSkipForward->  setIcon(factory->getIcon(IconFactory::Icon::skip_forward));
-    m_ui->buttonToggleSubList->setIcon(factory->getIcon(IconFactory::Icon::hamburger));
+    m_ui->buttonPlay->setIcon(
+        factory->getIcon(
+            m_paused ? IconFactory::Icon::play : IconFactory::Icon::pause
+        )
+    );
+    m_ui->buttonFullscreen->setIcon(
+        factory->getIcon(IconFactory::Icon::fullscreen)
+    );
+    m_ui->buttonSeekBackward->setIcon(
+        factory->getIcon(IconFactory::Icon::seek_backward)
+    );
+    m_ui->buttonSeekForward->setIcon(
+        factory->getIcon(IconFactory::Icon::seek_forward)
+    );
+    m_ui->buttonSkipBackward->setIcon(
+        factory->getIcon(IconFactory::Icon::skip_backward)
+    );
+    m_ui->buttonSkipForward->setIcon(
+        factory->getIcon(IconFactory::Icon::skip_forward)
+    );
+    m_ui->buttonToggleSubList->setIcon(
+        factory->getIcon(IconFactory::Icon::hamburger)
+    );
 
     m_ui->sliderProgress->setStyle(new SliderJumpStyle(QApplication::style()));
     m_ui->sliderVolume->setStyle(new SliderJumpStyle(QApplication::style()));
 }
 
+/* End Initializers */
+/* Begin Event Handlers */
+
+void PlayerControls::hideEvent(QHideEvent *event)
+{
+    QWidget::hideEvent(event);
+    Q_EMIT GlobalMediator::getGlobalMediator()->controlsHidden();
+}
+
+void PlayerControls::showEvent(QShowEvent *event)
+{
+    QWidget::showEvent(event);
+    Q_EMIT GlobalMediator::getGlobalMediator()->controlsShown();
+}
+
+/* End Event Handlers */
+/* Begin Slots */
+
 void PlayerControls::setDuration(const double value)
 {
     setPosition(0);
     m_duration = value;
-    m_startTime = -1;
-    m_endTime = -1;
     m_ui->sliderProgress->setRange(0, value);
     m_ui->labelTotal->setText(formatTime(value));
 }
@@ -129,37 +206,11 @@ void PlayerControls::setPosition(const double value)
 void PlayerControls::setPaused(const bool paused)
 {
     m_paused = paused;
-    m_ui->buttonPlay->setIcon(IconFactory::create()->getIcon(m_paused ? IconFactory::Icon::play : IconFactory::Icon::pause));
-}
-
-void PlayerControls::pauseResume()
-{
-    if (m_paused)
-    {
-        Q_EMIT GlobalMediator::getGlobalMediator()->controlsPlay();
-    }
-    else
-    {
-        Q_EMIT GlobalMediator::getGlobalMediator()->controlsPause();
-    }
-}
-
-void PlayerControls::setFullscreen(const bool value)
-{
-    if (value)
-    {
-        m_ui->buttonFullscreen->setIcon(IconFactory::create()->getIcon(IconFactory::Icon::restore));
-    }
-    else
-    {
-        m_ui->buttonFullscreen->setIcon(IconFactory::create()->getIcon(IconFactory::Icon::fullscreen));
-    }
-}
-
-void PlayerControls::toggleFullscreen()
-{
-    bool fullscreen = GlobalMediator::getGlobalMediator()->getPlayerAdapter()->isFullscreen();
-    Q_EMIT GlobalMediator::getGlobalMediator()->controlsFullscreenChanged(!fullscreen);
+    m_ui->buttonPlay->setIcon(
+        IconFactory::create()->getIcon(
+            m_paused ? IconFactory::Icon::play : IconFactory::Icon::pause
+        )
+    );
 }
 
 void PlayerControls::setVolumeLimit(const int64_t value)
@@ -176,7 +227,55 @@ void PlayerControls::setVolume(const int64_t value)
     m_ui->labelVolume->setText(volume);
 }
 
-QString PlayerControls::formatTime(const int time)
+void PlayerControls::setFullscreen(const bool value)
+{
+    if (value)
+    {
+        m_ui->buttonFullscreen->setIcon(
+            IconFactory::create()->getIcon(IconFactory::Icon::restore)
+        );
+    }
+    else
+    {
+        m_ui->buttonFullscreen->setIcon(
+            IconFactory::create()->getIcon(IconFactory::Icon::fullscreen)
+        );
+    }
+}
+
+/* End Slots */
+/* Begin Button Implementations */
+
+void PlayerControls::togglePause()
+{
+    if (m_paused)
+    {
+        Q_EMIT GlobalMediator::getGlobalMediator()->controlsPlay();
+    }
+    else
+    {
+        Q_EMIT GlobalMediator::getGlobalMediator()->controlsPause();
+    }
+}
+
+
+void PlayerControls::toggleFullscreen()
+{
+    GlobalMediator *mediator = GlobalMediator::getGlobalMediator();
+    bool fullscreen = mediator->getPlayerAdapter()->isFullscreen();
+    Q_EMIT mediator->controlsFullscreenChanged(!fullscreen);
+}
+
+/* End Button Implementations */
+
+
+#define SECONDS_IN_MINUTE   60
+#define SECONDS_IN_HOUR     3600
+#define FILL_SPACES         2
+#define BASE_TEN            10
+#define FILL_CHAR           '0'
+
+QString PlayerControls::formatTime(const int64_t time)
 {
     int hours   = time / SECONDS_IN_HOUR;
     int minutes = (time % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE;
@@ -193,12 +292,10 @@ QString PlayerControls::formatTime(const int time)
     return formatted;
 }
 
-void PlayerControls::hideEvent(QHideEvent *event)
-{
-    Q_EMIT GlobalMediator::getGlobalMediator()->controlsHidden();
-}
+#undef SECONDS_IN_MINUTE
+#undef SECONDS_IN_HOUR
+#undef FILL_SPACES
+#undef BASE_TEN
+#undef FILL_CHAR
 
-void PlayerControls::showEvent(QShowEvent *event)
-{
-    Q_EMIT GlobalMediator::getGlobalMediator()->controlsShown();
-}
+/* End Helpers */
