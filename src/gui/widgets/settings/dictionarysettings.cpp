1156ed4869ced8760bd64519ca58c7f55b3e9f29
Ripose <ripose@protonmail.com>
2021-06-29 11:28:30 -0700

    dictionarysettings: adds documentation

--- src/gui/widgets/settings/dictionarysettings.cpp
+++ src/gui/widgets/settings/dictionarysettings.cpp
@@ -21,37 +21,70 @@
 #include "dictionarysettings.h"
 #include "ui_dictionarysettings.h"
 
-#include "../../../util/globalmediator.h"
-#include "../../../util/iconfactory.h"
-#include "../../../util/constants.h"
-#include "../../../dict/dictionary.h"
-
+#include <QFileDialog>
 #include <QPushButton>
 #include <QSettings>
-#include <QFileDialog>
 #include <QThreadPool>
 
-DictionarySettings::DictionarySettings(QWidget *parent) : QWidget(parent), m_ui(new Ui::DictionarySettings)
+#include "../../../dict/dictionary.h"
+#include "../../../util/constants.h"
+#include "../../../util/globalmediator.h"
+#include "../../../util/iconfactory.h"
+
+/* Begin Constructor/Destructor */
+
+DictionarySettings::DictionarySettings(QWidget *parent)
+ : QWidget(parent),
+   m_ui(new Ui::DictionarySettings)
 {
     m_ui->setupUi(this);
 
-    refreshIcons();
+    initIcons();
 
-    connect(m_ui->buttonBox->button(QDialogButtonBox::StandardButton::Apply), &QPushButton::clicked,
-        this, &DictionarySettings::applySettings);
-    connect(m_ui->buttonBox->button(QDialogButtonBox::StandardButton::Reset), &QPushButton::clicked,
-        this, &DictionarySettings::refresh);
+    connect(
+        m_ui->buttonBox->button(QDialogButtonBox::StandardButton::Apply),
+        &QPushButton::clicked,
+        this,
+        &DictionarySettings::applySettings
+    );
+    connect(
+        m_ui->buttonBox->button(QDialogButtonBox::StandardButton::Reset),
+        &QPushButton::clicked,
+        this,
+        &DictionarySettings::restoreSaved
+    );
 
-    connect(m_ui->listDictionaries, &QListWidget::currentRowChanged, this,
-        [=] (int row) { setButtonsEnabled(row != -1); } );
+    connect(
+        m_ui->listDictionaries, &QListWidget::currentRowChanged,
+        this,                   [=] (int row) { setButtonsEnabled(row != -1); }
+    );
     
-    connect(m_ui->buttonUp,     &QToolButton::clicked, this, &DictionarySettings::moveUp);
-    connect(m_ui->buttonDown,   &QToolButton::clicked, this, &DictionarySettings::moveDown);
-    connect(m_ui->buttonAdd,    &QToolButton::clicked, this, &DictionarySettings::addDictionary);
-    connect(m_ui->buttonDelete, &QToolButton::clicked, this, &DictionarySettings::deleteDictionary);
+    connect(
+        m_ui->buttonUp, &QToolButton::clicked,
+        this,           &DictionarySettings::moveUp
+    );
+    connect(
+        m_ui->buttonDown, &QToolButton::clicked,
+        this,             &DictionarySettings::moveDown
+    );
+    connect(
+        m_ui->buttonAdd, &QToolButton::clicked,
+        this,            &DictionarySettings::addDictionary
+    );
+    connect(
+        m_ui->buttonDelete, &QToolButton::clicked,
+        this,               &DictionarySettings::deleteDictionary
+    );
 
-    connect(GlobalMediator::getGlobalMediator(), &GlobalMediator::dictionaryAdded,     this, &DictionarySettings::refresh);
-    connect(GlobalMediator::getGlobalMediator(), &GlobalMediator::requestThemeRefresh, this, &DictionarySettings::refreshIcons);
+    GlobalMediator *mediator = GlobalMediator::getGlobalMediator();
+    connect(
+        mediator, &GlobalMediator::dictionaryAdded,
+        this,     &DictionarySettings::restoreSaved
+    );
+    connect(
+        mediator, &GlobalMediator::requestThemeRefresh,
+        this,     &DictionarySettings::initIcons
+    );
 }
 
 DictionarySettings::~DictionarySettings()
@@ -60,7 +93,10 @@ DictionarySettings::~DictionarySettings()
     delete m_ui;
 }
 
-void DictionarySettings::refreshIcons()
+/* End Constructor/Destructor */
+/* Begin Initializers */
+
+void DictionarySettings::initIcons()
 {
     IconFactory *factory = IconFactory::create();
     m_ui->buttonAdd->   setIcon(factory->getIcon(IconFactory::Icon::plus));
@@ -69,22 +105,34 @@ void DictionarySettings::refreshIcons()
     m_ui->buttonDown->  setIcon(factory->getIcon(IconFactory::Icon::down));
 }
 
+/* End Initializers */
+/* Begin Event Handlers */
+
 void DictionarySettings::showEvent(QShowEvent *event)
 {
-    refresh();
+    QWidget::showEvent(event);
+    restoreSaved();
 }
 
 void DictionarySettings::hideEvent(QHideEvent *event)
 {
+    QWidget::hideEvent(event);
     setButtonsEnabled(false);
 }
 
-void DictionarySettings::refresh()
+/* End Event Handlers */
+/* Begin Button Box Handlers */
+
+void DictionarySettings::restoreSaved()
 {
     m_ui->listDictionaries->clear();
     m_ui->listDictionaries->addItems(
         GlobalMediator::getGlobalMediator()->getDictionary()->getDictionaries()
     );
+
+    /* Settings are applied in case there is a mismatch between the dictionaries
+     * in QSettings and the database.
+     */
     applySettings();
 }
 
@@ -100,12 +148,8 @@ void DictionarySettings::applySettings()
     settings.endGroup();
 }
 
-void DictionarySettings::setButtonsEnabled(const bool value)
-{
-    m_ui->buttonDelete->setEnabled(value);
-    m_ui->buttonDown->setEnabled(value);
-    m_ui->buttonUp->setEnabled(value);
-}
+/* End Button Box Handlers */
+/* Begin Move Button Handlers */
 
 void DictionarySettings::moveUp()
 {
@@ -129,16 +173,20 @@ void DictionarySettings::moveDown()
     }
 }
 
+/* End Move Button Handlers */
+/* Begin Add/Sub Buttons */
+
 void DictionarySettings::addDictionary()
 {
     QString file = QFileDialog::getOpenFileName(0, "Open the dictionary");
     if (file.isEmpty())
         return;
 
-    QThreadPool::globalInstance()->start([=]
-        {
+    QThreadPool::globalInstance()->start(
+        [=] {
             setEnabled(false);
-            Dictionary *dic = GlobalMediator::getGlobalMediator()->getDictionary();
+            Dictionary *dic =
+                GlobalMediator::getGlobalMediator()->getDictionary();
             QString err = dic->addDictionary(file);
             if (!err.isEmpty())
             {
@@ -156,7 +204,9 @@ void DictionarySettings::deleteDictionary()
     if (!m_ui->listDictionaries->count())
         return;
     
-    QListWidgetItem *item = m_ui->listDictionaries->takeItem(m_ui->listDictionaries->currentRow());
+    QListWidgetItem *item = m_ui->listDictionaries->takeItem(
+        m_ui->listDictionaries->currentRow()
+    );
     QSettings settings;
     settings.beginGroup(SETTINGS_DICTIONARIES);
     settings.remove(item->text());
@@ -164,7 +214,8 @@ void DictionarySettings::deleteDictionary()
     QThreadPool::globalInstance()->start(
         [=] {
             setEnabled(false);
-            Dictionary *dic = GlobalMediator::getGlobalMediator()->getDictionary();
+            Dictionary *dic =
+                GlobalMediator::getGlobalMediator()->getDictionary();
             QString err = dic->deleteDictionary(item->text());
             if (!err.isEmpty())
             {
@@ -177,3 +228,15 @@ void DictionarySettings::deleteDictionary()
         } 
     );
 }
+
+/* End Add/Sub Buttons */
+/* Begin Helpers */
+
+void DictionarySettings::setButtonsEnabled(const bool value)
+{
+    m_ui->buttonDelete->setEnabled(value);
+    m_ui->buttonDown  ->setEnabled(value);
+    m_ui->buttonUp    ->setEnabled(value);
+}
+
+/* End Helpers */
