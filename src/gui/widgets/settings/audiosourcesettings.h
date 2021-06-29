b27a9fcb44f1b03d5018363e1d1410c3afcda21e
Ripose <ripose@protonmail.com>
2021-06-28 20:17:26 -0700

    audiosourcesettings: add documentation

--- src/gui/widgets/settings/audiosourcesettings.h
+++ src/gui/widgets/settings/audiosourcesettings.h
@@ -28,35 +28,95 @@ namespace Ui
     class AudioSourceSettings;
 }
 
+/**
+ * Widget for adding remote audio sources.
+ */
 class AudioSourceSettings : public QWidget
 {
     Q_OBJECT
 
 public:
-    AudioSourceSettings(QWidget *parent = 0);
+    AudioSourceSettings(QWidget *parent = nullptr);
     ~AudioSourceSettings();
 
 protected:
+    /**
+     * Restores saved settings on show.
+     * @param event The show event, not used.
+     */
     void showEvent(QShowEvent *event) override;
 
 private Q_SLOTS:
+    /**
+     * Applys the current configuration to the settings.
+     */
     void applyChanges();
+
+    /**
+     * Restores saved settings to the UI without applying them.
+     */
     void restoreDefaults();
+
+    /**
+     * Restores saved settings to the UI without applying them.
+     */
     void restoreSaved();
+
+    /**
+     * Shows a help dialog.
+     */
     void showHelp();
+
+    /**
+     * Disables/Enables buttons based on the current selected row.
+     */
     void updateButtons();
+
+    /**
+     * Moves the currently select row up.
+     */
     void moveUp();
+
+    /**
+     * Moves the currently selected row down.
+     */
     void moveDown();
 
 private:
-    Ui::AudioSourceSettings *m_ui;
-
-    QString verifyNames() const;
+    /**
+     * Helper method for moving a row, step spaces.
+     * @param row  The row to move.
+     * @param step A signed number of rows to move the row.
+     */
     void moveRow(const int row, const int step);
+
+    /**
+     * Gaurentees the last row is empty and removes empty rows that are not the
+     * last row.
+     */
     void updateRows();
 
+    /**
+     * Verifies that names in the name column are not empty or dupicated.
+     * @return Error string. Empty string on no error.
+     */
+    QString verifyNames() const;
+
+    /**
+     * Returns if the item at row and col is empty.
+     * @param row The row of the item to check.
+     * @param col The column of the item to check.
+     */
     bool inline itemEmpty(const int row, const int col) const;
-    bool inline rowEmpty (const int row)                const;
+
+    /**
+     * Returns if the entire row is empty.
+     * @param row The row to check.
+     */
+    bool inline rowEmpty(const int row) const;
+
+    /* The UI object containing all the widgets. */
+    Ui::AudioSourceSettings *m_ui;
 };
 
 #endif // AUDIOSOURCESETTINGS_H
