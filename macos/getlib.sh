b82f7fed2cf25bb7e0004a2dde65b8e9df67202d
Ripose <ripose@protonmail.com>
2023-08-13 21:34:27 -0700

    macos/getlib: fix relative otool paths giving invalid library paths
    Homebrew changed some stuff up so that the libraries cmake finds are
    symlinks to actual dylibs. This is complicated further by otool
    returning paths relative to the actual file and not the link. This fixes
    the issue by resolving the link, then using otool, then fixing any
    relative paths.

--- macos/getlib.sh
+++ macos/getlib.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script find the otool path of a specific library and outputs it to stdout
 if [ "$#" -ne 2 ]
@@ -8,4 +8,29 @@ then
     exit 1
 fi
 
-otool -L "$1" | grep -oE '(\/.+?) ' | grep "$2" | xargs | tr -d '\n'
+# https://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+# Resolive symlinks
+TARGET_FILE=$1
+
+cd `dirname $TARGET_FILE`
+TARGET_FILE=`basename $TARGET_FILE`
+
+# Iterate down a (possible) chain of symlinks
+while [ -L "$TARGET_FILE" ]
+do
+    TARGET_FILE=`readlink $TARGET_FILE`
+    cd `dirname $TARGET_FILE`
+    TARGET_FILE=`basename $TARGET_FILE`
+done
+
+# Compute the canonicalized name by finding the physical path
+# for the directory we're in and appending the target file.
+LIB_DIR=`pwd -P`
+LIB_PATH="$LIB_DIR/$TARGET_FILE"
+
+PATH=$(otool -L "$LIB_PATH" | grep -oE '(\/.+?) ' | grep "$2" | xargs | tr -d '\n')
+if [[ $PATH == /..* ]]
+then
+    PATH="$LIB_DIR/$PATH"
+fi
+echo -n "$PATH"
